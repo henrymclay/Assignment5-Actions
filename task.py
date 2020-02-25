41284522a79656c0e10bfc9f78657bc3fb33648f
Henry <henry@crushroom.com>
2020-02-24 20:57:12 -0800

    list test

--- task.py
+++ task.py
@@ -17,3 +17,11 @@ def circleArea(radius):
     pi = math.pi
     area = (pi * radius * radius)
     return area
+
+
+def listFirstLast(input_list):
+    return_list = []
+    return_list.append(input_list[0])
+    #return_list.reverse()
+    return_list.append(input_list[-1])
+    return return_list
--- tests.py
+++ tests.py
@@ -32,3 +32,19 @@ class TestCase(unittest.TestCase):
         self.assertEqual(expected2, task.circleArea(2))
         self.assertEqual(expected3, task.circleArea(3))
         self.assertEqual(expected0, task.circleArea(0))
+
+    def testList(self):
+        list1 = [1,2]
+        list2 = [1,2,3]
+        list3 = [3]
+        list4 = [1,2,3,4]
+
+        expected1 = [1,2]
+        expected2 = [1,3]
+        expected3 = [3,3]
+        expected4 = [1,4]
+
+        self.assertEqual(expected1, task.listFirstLast(list1))
+        self.assertEqual(expected2, task.listFirstLast(list2))
+        self.assertEqual(expected3, task.listFirstLast(list3))
+        self.assertEqual(expected4, task.listFirstLast(list4))
