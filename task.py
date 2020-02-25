27bb85987e89d75e09da77d4cc98f07e9854d896
Henry <henry@crushroom.com>
2020-02-24 20:38:39 -0800

    circle test

--- task.py
+++ task.py
@@ -6,6 +6,14 @@ date: 2/24/20
 an automated testing assignment! file 1/2
 """
 
+import math
+
 
 def firstrun():
     return "success"
+
+
+def circleArea(radius):
+    pi = math.pi
+    area = (pi * radius * radius)
+    return area
--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ an automated testing assignment! file 2/2
 
 import unittest
 import task
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -19,3 +20,15 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def testCircle(self):
+        pi = math.pi
+
+        expected1 = pi
+        expected2 = pi * 4
+        expected3 = pi * 9
+        expected0 = 0
+        self.assertEqual(expected1, task.circleArea(1))
+        self.assertEqual(expected2, task.circleArea(2))
+        self.assertEqual(expected3, task.circleArea(3))
+        self.assertEqual(expected0, task.circleArea(0))
